21a26ee27977ef01973741084d3cee77223128a3
Oshchenkov <oshchenkovv@gmail.com>
2019-10-10 19:37:01 +0300

    rewrite js scripts and add burger menu for mobile to show sidebar

--- docs/js/common.js
+++ docs/js/common.js
@@ -1,19 +1,45 @@
 "use strict";
 
-const animationDuration = 200;
+document.addEventListener("DOMContentLoaded", () => {
+  const animationDuration = 200;
+  const $sidebar = $(".sidebarNavContainer");
+  const $sidebarBtn = $(".sidebarNavContainer-button");
 
-// hide dropList-ul on click outside
-$(document).click(function(event) {
-  let $target = $(event.target);
+  // hide  on click outside
+  document.addEventListener("click", event => {
+    const $target = $(event.target);
+    console.log("event.target: ", event.target);
 
-  if (!$target.closest(".dropList").length && $(".dropList").is(":visible")) {
-    $(".dropList-ul").slideUp(animationDuration);
-  }
-});
+    //dropList-ul
+    if (!$target.closest(".dropList").length && $(".dropList").is(":visible")) {
+      $(".dropList-ul").slideUp(animationDuration);
+    }
 
-// dropList hide/show
-$(".dropList").click(function(event) {
-  let ulList = $(this).find(".dropList-ul")[0];
+    // Sidebar
+    if (
+      !$target.closest($sidebar).length &&
+      !$target.closest($sidebarBtn).length &&
+      $($sidebar).is(":visible")
+    ) {
+      $sidebar.slideUp(animationDuration);
+      $sidebarBtn.slideDown(animationDuration);
+    }
+  });
 
-  $(ulList).slideToggle(animationDuration);
+  // dropList hide/show
+  $(".dropList").click(event => {
+    const ulList = $(event.currentTarget).find(".dropList-ul")[0];
+
+    $(ulList).slideToggle(animationDuration);
+  });
+
+  // Show Sidebar
+  $sidebarBtn.click(event => {
+    $sidebar.slideDown(animationDuration);
+    if ($sidebar.is(":visible")) {
+      $sidebarBtn.slideUp(animationDuration);
+    }
+  });
+
+  //
 });
